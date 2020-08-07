484650f64f356bc538f5a405d45363c88178bdde
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-07 22:59:39 +0900

    feat: urlの繋ぎ込み

---
+++ api/urls.py
@@ -0,0 +1,43 @@
+from rest_framework import routers
+from django.urls import path
+from django.conf.urls import include, re_path
+from store.views import store
+
+# drf_yasg code starts here
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+  openapi.Info(
+    title="Ecsite API",
+    default_version='v1.0.0', # 必須APIドキュメントのバージョン情報
+    description="Welcome to test Ecsite",# APIの詳細説明を記載
+    terms_of_service="https://www.jaseci.org",# サービス利用規約
+    contact=openapi.Contact(# 連絡先情報を定義
+      email="jason@jaseci.org", 
+      name="Customer Support"),
+    license=openapi.License(name="Awesome IP"),# ライセンス名 urlも定義できる。イセンス名はSPDXを参考にするのがオススメ
+  ),
+  public=True,
+  permission_classes=(permissions.AllowAny,),
+)
+# ends here
+
+# ViewSet
+router = routers.DefaultRouter()
+router.register('', )
+
+router = routers.DefaultRouter()
+router.register('', )
+
+
+urlpatterns = [
+  re_path('swagger(?P<format>\.json|\.yaml)$',
+    schema_view.without_ui(cache_timeout=0), name='schema-json'),
+  path('swagger/', 
+    schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+  path('redoc/', 
+    schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+  path('', include(router.urls))
+]
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+# image
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
+
+# url設定--url⇨root⇨image
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

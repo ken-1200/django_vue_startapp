aed9a9e94acdbf50490d902900b1e10fe305c53e
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-16 01:59:26 +0900

    add: django管理画面にItem追加

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from store.models.stores import Store
+from item.models.items import Item
 
 # Register your models here.
 admin.site.register(Store)
+admin.site.register(Item)

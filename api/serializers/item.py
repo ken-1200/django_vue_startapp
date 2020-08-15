f75073250b2f959bf6a964ba62ae0eee51098608
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-16 02:27:32 +0900

    feat: ItemSerializer

---
+++ api/serializers/item.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from item.models.items import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Item
+    fields = ['id', 'store_id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total', 'created_at']
+
+# create
+  def create(self, validated_data):
+    print('New Item Created')
+    item = Item(
+      store_id=validated_data['store_id'],
+      item_name=validated_data['item_name'],
+      item_detail=validated_data['item_detail'],
+      item_price=validated_data['item_price'],
+      item_total=validated_data['item_total'],
+    )
+    item.save()
+    return item
+
+# update-partial_update
+  def update(self, instance, validated_data):
+    print('update - partial_update')
+    instance.item_name = validated_data.get('item_name', instance.item_name)
+    instance.item_img = validated_data.get('item_img', instance.item_img)
+    instance.item_detail = validated_data.get('item_detail', instance.item_detail)
+    instance.item_price = validated_data.get('item_price', instance.item_price)
+    instance.item_total = validated_data.get('item_total', instance.item_total)
+    instance.save()
+    return instance

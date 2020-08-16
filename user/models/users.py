24195453e26820223754be0788d2b0ac1a0fe555
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-16 16:46:37 +0900

    update: 退会日時に変更

--- user/models/users.py
+++ user/models/users.py
@@ -6,7 +6,7 @@ from django.db import models
 class User(models.Model):
   user_email = models.EmailField(max_length=254, null=False, blank=False, unique=True)
   user_password = models.CharField(max_length=30, null=False, blank=False)
-  withdrawal_datetime = models.DateTimeField(auto_now_add=True, null=True)
+  withdrawal_datetime = models.DateTimeField('DeleteDate', blank=True, null=True, auto_now=True)
 
   def __str__(self):
     return self.user_email

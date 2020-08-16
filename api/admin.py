e5170c922d68f1d559cf6ace277da87e56b1633a
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-16 23:38:22 +0900

    add: Django管理画面追加

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from store.models.stores import Store
 from item.models.items import Item
 from user.models.users import User
+from payment.models.payments import Payment
 
 # Register your models here.
 admin.site.register(Store)
 admin.site.register(Item)
 admin.site.register(User)
+admin.site.register(Payment)

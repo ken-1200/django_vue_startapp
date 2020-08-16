ca4cc7fcf49e73b55964e18b900d34ddc298581f
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-16 16:45:32 +0900

    add: django管理画面

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from store.models.stores import Store
 from item.models.items import Item
+from user.models.users import User
 
 # Register your models here.
 admin.site.register(Store)
 admin.site.register(Item)
+admin.site.register(User)

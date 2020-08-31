7f47371d9588d575249b51a153c182cc345fac30
Kensuke Machida <ken@MacBook-Pro.local>
2020-08-31 19:09:45 +0900

    add: 管理画面にrole追加

--- api/admin.py
+++ api/admin.py
@@ -3,9 +3,11 @@ from store.models.stores import Store
 from item.models.items import Item
 from user.models.users import User
 from payment.models.payments import Payment
+from payment.models.roles import Role
 
 # Register your models here.
 admin.site.register(Store)
 admin.site.register(Item)
 admin.site.register(User)
 admin.site.register(Payment)
+admin.site.register(Role)

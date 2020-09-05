c9a49374e5d0c769dac79565181baed054dd96d1
Kensuke Machida <ken@MacBook-Pro.local>
2020-09-05 23:05:01 +0900

    feat: カラム変更

--- store/models/stores.py
+++ store/models/stores.py
@@ -7,6 +7,8 @@ class Store(models.Model):
   store_name = models.CharField(max_length=30, null=False, blank=False)
   store_email = models.EmailField(max_length=254, null=False, blank=False)
   store_password = models.CharField(max_length=30, null=False, blank=False)
+  created_at = models.DateTimeField(auto_now_add=True, null=True)
+  updated_at = models.DateTimeField(auto_now=True)
   deleted_at = models.DateTimeField(null=True)
 
   def __str__(self):
--- user/models/users.py
+++ user/models/users.py
@@ -6,6 +6,8 @@ from django.db import models
 class User(models.Model):
   user_email = models.EmailField(max_length=254, null=False, blank=False)
   user_password = models.CharField(max_length=30, null=False, blank=False)
+  created_at = models.DateTimeField(auto_now_add=True, null=True)
+  updated_at = models.DateTimeField(auto_now=True)
   withdrawal_at = models.DateTimeField(null=True)
 
   def __str__(self):

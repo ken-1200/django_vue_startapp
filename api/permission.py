6e6ca01979ab44fc4815134beec4aa27ebacff4f
Kensuke Machida <ken@MacBook-Pro.local>
2020-11-16 22:12:08 +0900

    feat: アイテムの権限付与

---
+++ api/permission.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+from rest_framework import exceptions
+
+# item権限機能
+class CustomItemPermission(permissions.BasePermission):
+  def has_permission(self, request, view):
+    print('認証されました。')
+    return super().has_permission(request, view)
+
+  """
+  オーナーのみ編集を可能にするカスタム権限
+  """
+  def has_object_permission(self, request, view, obj):
+    # メソッドがGET, HEAD もしくは OPTIONSは常に許可
+    if request.method in permissions.SAFE_METHODS:
+      return True
+    # 権限があるユーザーのみ編集可
+    print('権限があるユーザーが編集しました。')
+    return obj.store_owner == request.user

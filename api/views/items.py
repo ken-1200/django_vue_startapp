109f64ce56b4a522825a300fc2b5b8344f97e8d3
Kensuke Machida <ken@MacBook-Pro.local>
2020-11-16 22:14:25 +0900

    feat: 新エンドポイント

--- api/views/items.py
+++ api/views/items.py
@@ -7,9 +7,42 @@ from item.models.items import Item
 from django.utils import timezone
 from rest_framework.exceptions import APIException
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from api.permission import CustomItemPermission
+from api.authentication import CustomAuthentication
+from rest_framework import permissions, generics
+from store.models.stores import Store
 
 # Create your views here.
+
+# 商品一覧
+# モデルインスタンスのコレクションを表すための読み取り/書き込みエンドポイントに使用される get/post
+class ItemList(generics.ListCreateAPIView):
+  """
+  アイテム GET(ALL), POST
+  """
+  queryset = Item.objects.all()
+  serializer_class = ItemSerializer
+  authentication_classes = [CustomAuthentication,]
+  # 認証されたユーザのリクエストは読み書きが可能になり、認証されていないリクエストは読み取りのみ可能になります。
+  permission_classes = [CustomItemPermission,]
+
+  # 新しいオブジェクトインスタンスを保存するときに呼び出される
+  def perform_create(self, serializer):
+    print('認証されたユーザーで商品を作成します。')
+    serializer.save(store_owner=self.request.user)
+
+# 商品詳細クラス
+# 単一のモデルインスタンスを表すための読み取り-書き込み-削除エンドポイントに使用されます。
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+  """
+  アイテム GET(pk指定), PUT, PATCH, DELETE(レコード削除)
+  """
+  queryset = Item.objects.all()
+  serializer_class = ItemSerializer
+  authentication_classes = [CustomAuthentication,]
+  # 編集や削除は作成者のみが行える
+  permission_classes = [CustomItemPermission, ]
 
 # APIException
 class NotFound(APIException):
@@ -20,14 +53,18 @@ class NotFound(APIException):
 # ItemViewSetの作成
 class ItemViewSet(viewsets.ModelViewSet):
   """
-  API endpoint that allows items to be viewed or edited.
+  アイテム GET(削除された商品以外), DELETE(レコードは残る)
   """
-  # パーミッション解除
-  permission_classes = ()
+  # パーミッション設定
+  # authentication_classesで使用する認証クラスを指定
+  # permission_classesにはこのAPIを使用するための権限を設定
+  # IsAuthenticatedはauthentication_classesで設定した認証が行えた場合にこのAPIにアクセス可能
+  authentication_classes = [CustomAuthentication,]
+  permission_classes = [CustomItemPermission,]
   queryset = Item.objects.all()
   serializer_class = ItemSerializer
 
-# 存在する商品List取得
+# 存在する商品(削除されたもの以外全て)List取得
   @action(detail=False, methods=['get'])
   def get_items(self, request):
     try:
@@ -45,7 +82,7 @@ class ItemViewSet(viewsets.ModelViewSet):
       })
     return HttpResponse(content=item_list, content_type="application/json", status=200)
 
-# delete_endpoint
+# 完全削除はせずに各値に初期値を入れて、削除
   @action(detail=True, methods=['delete'])
   def delete_item(self, request, pk=None):
     # itemボタン押下時に、削除日時に押下時の時間を入れる。

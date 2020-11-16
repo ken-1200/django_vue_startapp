ab7382226fb9ce393cb544b90c95dd0459c0064d
Kensuke Machida <ken@MacBook-Pro.local>
2020-11-16 22:12:51 +0900

    feat: 編集

--- api/serializers/item.py
+++ api/serializers/item.py
@@ -2,28 +2,9 @@ from rest_framework import serializers
 from item.models.items import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+  # Storeモデルのストア名をとってくる
+  store_owner = serializers.ReadOnlyField(source='store_owner.store_name')
   class Meta:
     model = Item
-    fields = ['id', 'store_id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total']
+    fields = ['id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total', 'store_owner']
 
-# create
-  def create(self, validated_data):
-    item = Item(
-      store_id=validated_data['store_id'],
-      item_name=validated_data['item_name'],
-      item_detail=validated_data['item_detail'],
-      item_price=validated_data['item_price'],
-      item_total=validated_data['item_total'],
-    )
-    item.save()
-    return item
-
-# update-partial_update
-  def update(self, instance, validated_data):
-    instance.item_name = validated_data.get('item_name', instance.item_name)
-    instance.item_img = validated_data.get('item_img', instance.item_img)
-    instance.item_detail = validated_data.get('item_detail', instance.item_detail)
-    instance.item_price = validated_data.get('item_price', instance.item_price)
-    instance.item_total = validated_data.get('item_total', instance.item_total)
-    instance.save()
-    return instance

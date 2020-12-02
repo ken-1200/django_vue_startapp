b605b3623215f14c82bb30f97979b4d55d93a935
Kensuke Machida <ken@MacBook-Pro.local>
2020-12-03 01:43:11 +0900

    feat:ログイン系機能

---
+++ src/views/StoreLogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="store-login">
+      <div class="store-login__wrap">
+        <div class="store-login__title">
+          <h1>ログイン画面</h1>
+          <Login/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from '../components/Login.vue'
+
+export default {
+  components: {
+    Login,
+  }
+}
+</script>

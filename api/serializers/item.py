5eabe549b9c15d8b8f1339547929dd3df4c2ab04
Kensuke Machida <ken@MacBook-Pro.local>
2020-12-03 01:38:49 +0900

    feat:商品取得API作成

--- api/serializers/item.py
+++ api/serializers/item.py
@@ -2,9 +2,8 @@ from rest_framework import serializers
 from item.models.items import Item
 
 class ItemSerializer(serializers.ModelSerializer):
-  # Storeモデルのストア名をとってくる
-  store_owner = serializers.ReadOnlyField(source='store_owner.store_name')
+  # Storeモデルのストアidをとってくる
+  store_owner = serializers.ReadOnlyField(source='store_owner.group_ptr_id')
   class Meta:
     model = Item
     fields = ['id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total', 'store_owner']
-
--- api/views/items.py
+++ api/views/items.py
@@ -53,7 +53,7 @@ class NotFound(APIException):
 # ItemViewSetの作成
 class ItemViewSet(viewsets.ModelViewSet):
   """
-  アイテム GET(削除された商品以外), DELETE(レコードは残る)
+  アイテム GET(ストアオーナーに紐づいたアイテム一覧), アイテム GET(削除された商品以外全ての一覧), PATCH(item_idに紐づく商品を更新する), DELETE(レコードは残る)
   """
   # パーミッション設定
   # authentication_classesで使用する認証クラスを指定
@@ -64,9 +64,82 @@ class ItemViewSet(viewsets.ModelViewSet):
   queryset = Item.objects.all()
   serializer_class = ItemSerializer
 
+# ストアオーナーに紐づいたアイテムを取得する
+  @action(detail=False, methods=['get'])
+  def get_item_detail(self, request):
+    try:
+      # deleted_at=Noneの商品をJsonにシリアル化
+      item = Item.objects.filter(store_owner=request.user.id)
+      item_list = serializers.serialize('json', item)
+    except Exception as err:
+      # システム終了以外の全ての組み込み例外
+      print(err)
+      raise NotFound({
+        'NOT_FOUND': [
+          NotFound().status_code,
+          NotFound().default_detail,
+        ]
+      })
+    return HttpResponse(content=item_list, content_type="application/json", status=200)
+
+# 
+  @action(detail=True, methods=['patch'])
+  def update_item(self, request, pk=None):
+    # 商品がある場合商品を更新する
+    if not Item.objects.filter(store_owner=request.user.id):
+      # オーナーの商品がない場合
+      return Response({'message': 'あなたの商品が存在しません。'}, status=403)
+
+    # リクエストデータを取得する
+    try:
+      ob = request.data
+      item_id = ob['item_id']
+      item_name = ob['item_name']
+      item_detail = ob['item_detail']
+      item_price = ob['item_price']
+      item_total = ob['item_total']
+      item_img = ob['item_img']
+    except:
+      return Response({'message': '読み込みに失敗しました。'}, status=400)
+
+    try:
+      # 商品アップデート
+      item_obj = self.queryset.get(pk=ob['item_id'])
+      item_obj.id = item_id
+      item_obj.item_name = item_name
+      item_obj.item_img = item_img
+      item_obj.item_detail = item_detail
+      item_obj.item_price = item_price
+      item_obj.item_total = item_total
+      item_obj.updated_at = timezone.now()
+      item_obj.save()
+
+      # レスポンスデータ
+      content = {
+        'item_id': item_obj.id,
+        'item_name': item_obj.item_name,
+        'item_detail': item_obj.item_detail,
+        'item_price': item_obj.item_price,
+        'item_total': item_obj.item_total,
+        'updated_at': item_obj.updated_at,
+        'item_img': '{item_obj.item_img}'.format(item_obj=item_obj),
+      }
+
+    except Exception as err:
+      # システム終了以外の全ての組み込み例外
+      print(err)
+      raise NotFound({
+        'NOT_FOUND': [
+          NotFound().status_code,
+          NotFound().default_detail,
+        ]
+      })
+    return Response({'message': 'Success', 'data': content, 'status': 201})
+
+
 # 存在する商品(削除されたもの以外全て)List取得
   @action(detail=False, methods=['get'])
-  def get_items(self, request):
+  def get_items_list(self, request):
     try:
       # deleted_at=Noneの商品をJsonにシリアル化
       item = Item.objects.filter(deleted_at=None)
@@ -87,7 +160,7 @@ class ItemViewSet(viewsets.ModelViewSet):
   def delete_item(self, request, pk=None):
     # itemボタン押下時に、削除日時に押下時の時間を入れる。
     try:
-      item_obj = self.get_object()
+      item_obj = self.queryset.get(pk=request.data['item_id'])
       item_obj.item_name = ''
       item_obj.item_img = ''
       item_obj.item_detail = ''

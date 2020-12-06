d9b07e8057a99b35a25e814f355719b1a201dcfe
Kensuke Machida <ken@MacBook-Pro.local>
2020-12-06 23:20:46 +0900

    feat: ログインVue追加

---
+++ src/views/User/UserLogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div>
+      <div>
+        <div>
+          <h1>Userログイン画面</h1>
+          <UserLogin/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserLogin from '../../components/UserLogin.vue'
+
+export default {
+  components: {
+    UserLogin,
+  }
+}
+</script>

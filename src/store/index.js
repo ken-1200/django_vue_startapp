7373e43002f1b03a47e1983bb30c40a5ae7574a1
Your Name <you@example.com>
2021-01-05 01:06:02 +0900

    feat: ペイメント機能追加

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,12 @@ export default new Vuex.Store({
     user_access_token: null,
     store_id: null,
     user_id: null,
-    item_data: [],
+    userInfo: [],
+    storeItemData: [],
+    allItemListData: [],
+    addToCart: [],
+    itemInfo: [],
+    paymentInfo: [],
     error: null,
   },
   getters: {
@@ -19,7 +24,15 @@ export default new Vuex.Store({
     user_access_token: state => state.user_access_token,
     store_id: state => state.store_id,
     user_id: state => state.user_id,
-    item_data: state => state.item_data,
+    userInfo: state => state.userInfo,
+    storeItemData: state => state.storeItemData,
+    allItemListData: state => state.allItemListData,
+    selectItems: state => id => {
+      return state.allItemListData.filter(el => el.pk === id);
+    },
+    addToCart: state => state.addToCart,
+    itemInfo: state => state.itemInfo,
+    paymentInfo: state => state.paymentInfo,
     error: state => state.error,
   },
   mutations: {
@@ -39,14 +52,52 @@ export default new Vuex.Store({
     updateUserId(state, user_id) {
       state.user_id = user_id;
     },
-    // 商品データ
-    getItemDetail(state, item_data) {
-      state.item_data = item_data;
+    // ストア側商品データ
+    getItemDetail(state, storeItemData) {
+      state.storeItemData = storeItemData;
+    },
+    // 商品リスト
+    getAllItemListData(state, allItemListData) {
+      state.allItemListData = allItemListData;
+    },
+    // カートの中身
+    addToCart(state, addToCart) {
+      state.addToCart.push(addToCart);
+    },
+    // 商品情報
+    getItemInfo(state, itemInfo) {
+      state.itemInfo.push(itemInfo);
+    },
+    // カート内削除
+    deleteCart(state, id) {
+      const index = state.addToCart.findIndex(el => el.pk == id);
+      if (index >= 0) {
+        state.addToCart.splice(index, 1);
+      }
+    },
+    // カート内の情報削除
+    deleteCartInfo(state, id) {
+      const index = state.itemInfo.findIndex(el => el.item_id == id);
+      if (index >= 0) {
+        state.itemInfo.splice(index, 1);
+      }
+    },
+    // ユーザー情報を格納
+    getUserInfo(state, userInfo) {
+      state.userInfo = userInfo;
+    },
+    // 支払い情報取得
+    getPaymentInfo(state, paymentInfo) {
+      state.paymentInfo = paymentInfo;
     }
   },
   actions: {
     // オートログイン
     async userAutoLogin({ commit, dispatch }) {
+      // ローカルストレージからユーザー情報を取得する
+      const userName = localStorage.getItem('userName');
+      const userEmail = localStorage.getItem('userEmail');
+
       // ローカルストレージからユーザーidを取得する
       const userId = localStorage.getItem('userId');
 
@@ -86,29 +137,48 @@ export default new Vuex.Store({
 
         // ユーザーidをステートに保存する
         commit('updateUserId', userId);
+
+        // ユーザー情報をステートに保存する
+        const userInfo = {
+          user_name: userName,
+          user_email: userEmail,
+        }
+        commit('getUserInfo', userInfo);
       }
     },
     // ユーザーログイン
-    userLogin({ dispatch }, loginData) {
-      axios.post('/user_login/', {
+    async userLogin({ dispatch }, loginData) {
+      await axios.post('/user_login/', {
         user_email: loginData.user_email,
         password: loginData.password,
       })
       .then(response => {
         console.log(response.data.data);
+
+        // user_id
+        const id = response.data.data.user_id;
+
         dispatch('setUserAuthData', {
-          // オブジェクトでid, 有効期限、アクセス、リフレッシュトークンを渡す
+          // オブジェクトでユーザー名、メールアドレス、id, 有効期限、アクセス、リフレッシュトークンを渡す
+          user_name: response.data.data.user_name,
+          user_email: response.data.data.user_email,
           user_id: response.data.data.user_id,
           access_token: response.data.data.access_token,
           refresh_token: response.data.data.refresh_token,
           expires_in: response.data.data.expires_in,
         });
+
+        if (this.getters.addToCart.length != 0) {
+          // カートに商品が入っている場合は支払い確認画面
+          router.push(`/order/cart/kinomo/${id}`);
+        } else {
+          // 商品一覧画面
+          router.push('/user_home');
+        }
       })
       .catch(error => {
         console.log(error);
-      })
-      // ここ修正
-      router.push('/item_list');
+      });
     },
     // トークンをリフレッシュする為の関数
     async userRefreshAccessToken({ dispatch }, refreshToken) {
@@ -119,7 +189,9 @@ export default new Vuex.Store({
       .then(response => {
         // 更新されたaccess_tokenが帰ってくる
         dispatch('setUserAuthData', {
-          // オブジェクトで有効期限、アクセス、リフレッシュトークンを渡す
+          // オブジェクトでユーザー名、メールアドレス、id, 有効期限、アクセス、リフレッシュトークンを渡す
+          user_name: response.data.data.user_name,
+          user_email: response.data.data.user_email,
           user_id: response.data.data.user_id,
           access_token: response.data.data.access_token,
           refresh_token: response.data.data.refresh_token,
@@ -140,7 +212,16 @@ export default new Vuex.Store({
       // mutationsを実行し、ステートのユーザーidに保存する
       commit('updateUserId', authData.user_id);
 
-      // ローカルストレージにアクセストークンと有効期限時間とリフレッシュトークンを保存する(有効期限(12H)も保存)
+      // ユーザー情報を格納
+      const userInfo = {
+        user_name: authData.user_name,
+        user_email: authData.user_email,
+      }
+      commit('getUserInfo', userInfo);
+
+      // ローカルストレージにユーザー名、メールさドレス、id、アクセストークンと有効期限時間とリフレッシュトークンを保存する(有効期限(12H)も保存)
+      localStorage.setItem('userName', authData.user_name);
+      localStorage.setItem('userEmail', authData.user_email);
       localStorage.setItem('userId', authData.user_id);
       localStorage.setItem('userRefreshAccessToken', authData.access_token);
       localStorage.setItem('userRefreshToken', authData.refresh_token);
@@ -149,7 +230,7 @@ export default new Vuex.Store({
       // リフレッシュトークンを使って、1時間置きにトークンを更新する
       setTimeout(() => {
         dispatch('userRefreshAccessToken', authData.refresh_token);
-        console.log('1時間おきに更新しました。')
+        console.log('1時間おきに更新しました。');
       }, authData.expires_in * 1000);
     },
     // ログアウト
@@ -160,12 +241,81 @@ export default new Vuex.Store({
       // userIdを削除
       commit('updateUserId', null);
 
+      // ユーザー情報を削除
+      commit('getUserInfo', null);
+
       // ローカルストレージから各アイテムを削除
+      localStorage.removeItem('userName');
+      localStorage.removeItem('userEmail');
       localStorage.removeItem('userId');
       localStorage.removeItem('userRefreshAccessToken');
       localStorage.removeItem('userRefreshToken');
       localStorage.removeItem('userExpiryTimeMs');
+
+      // DashBoardに遷移する
+      router.push('/');
     },
+    // 商品リスト取得
+    async getItemList({ commit }) {
+      await axios.get('/all/items_list/')
+      .then(response => {
+        console.log(response);
+        // コミット実行で商品リストを格納
+        commit('getAllItemListData', response.data);
+      })
+      .catch(error => {
+        // エラー処理
+        this.getters.error = `商品が見つかりませんでした。${error}`;
+        console.log(error);
+      })
+    },
+    // カートの処理/制御
+    addToCart({ state, commit }, itemData) {
+      // idに紐づく商品情報を取得する
+      const data = state.allItemListData.filter(el => el.pk == itemData.item_id);
+
+      // 同じ商品をカートに入れた時は既に入っているという旨の警告を出す
+      const sameItem = state.addToCart.find(el => el.pk == itemData.item_id);
+
+      if (!sameItem) {
+        // カートに格納
+        commit('addToCart', data[0]);
+
+        // 数量を格納
+        commit('getItemInfo', itemData);
+
+        // ページ遷移
+        router.push('/order/cart/kinomo');
+      } else {
+        // アラートを表示
+        itemData.alert = true;
+
+        // 1分後解除
+        if (itemData.alert) {
+          setTimeout(() => {
+            itemData.alert = false;
+          }, 60000);
+        }
+      }
+
+      // カートに入れた後の商品一覧の在庫数を減らす
+      const quantity = state.allItemListData.find(el => el.pk == itemData.item_id);
+
+      // 商品一覧の在庫数からカート内の商品数を引く
+      quantity.fields.item_total -= itemData.item_total;
+    },
+    // カート内削除
+    deleteCart({ commit }, id) {
+      commit('deleteCart', id);
+    },
+    // カート内の情報削除
+    deleteCartInfo({ commit }, id) {
+      commit('deleteCartInfo', id);
+    },
+    // 購入する
+    // payment() {
+      
+    // }
 
 /* ストア側の処理
   - 商品取得
@@ -239,8 +389,8 @@ export default new Vuex.Store({
       }
     },
     // ログイン
-    login({ dispatch }, loginData) {
-      axios.post('store_login/', {
+    async login({ dispatch }, loginData) {
+      await axios.post('store_login/', {
         store_email: loginData.store_email,
         store_password: loginData.store_password
       })
@@ -253,11 +403,14 @@ export default new Vuex.Store({
           refresh_token: response.data.data.refresh_token,
           expires_in: response.data.data.expires_in,
         });
+        // ストアホーム画面に遷移
+        router.push('/store_home');
       })
       .catch(error => {
+        // ここもう少し改善余地あり1/3
         console.log(error);
+        router.push('/store_register');
       });
-      router.push('/');
     },
     // ログアウト
     logout({ commit }) {
@@ -276,7 +429,7 @@ export default new Vuex.Store({
       localStorage.removeItem('expiryTimeMs');
       localStorage.removeItem('refreshToken');
 
-      // StoreDashBoardに遷移する
+      // DashBoardに遷移する
       router.push('/');
     },
     // トークンをリフレッシュする為の関数

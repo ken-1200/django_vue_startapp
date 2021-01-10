b68118e61cbce52901d1547078ab8e98a9a5d8bc
Your Name <you@example.com>
2021-01-10 15:54:21 +0900

    feat: 画像をBase64で受け取る

--- api/serializers/item.py
+++ api/serializers/item.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from item.models.items import Item
+from drf_extra_fields.fields import Base64ImageField
 
 class ItemSerializer(serializers.ModelSerializer):
   # Storeモデルのストアidをとってくる
   store_owner = serializers.ReadOnlyField(source='store_owner.group_ptr_id')
+
+  # 画像をBase64で受け取る(decodeする)
+  item_img = Base64ImageField()
   class Meta:
     model = Item
     fields = ['id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total', 'store_owner']

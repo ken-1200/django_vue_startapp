8189690f92ea1596608fd389b691d752515e266b
Your Name <you@example.com>
2021-01-12 04:06:20 +0900

    feat: ユーザー情報取得

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from user.models.users import User, CustomUserManager
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.hashers import check_password, make_password
 
 class UserSerializer(serializers.ModelSerializer):
   password = serializers.CharField(write_only=True, required=False)
@@ -10,15 +11,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 # パスワード更新
   def update(self, instance, validated_data):
-    if 'password' in validated_data:
-      # 更新の場合 set_passwordでハッシュ化
-      instance.set_password(validated_data['password'])
-      print('更新しました。')
-    else:
-      # 親クラス(defaultのupdateメソッド)を使う
-      instance.super().update(instance, validated_data)
-      print('更新しませんでした。')
+    exist_password = instance.password
+    valid_password = validated_data.get('password', instance.password)
+
+    # パスワードチェック
+    if check_password(valid_password, exist_password):
+      # 同じパスワードの時 既存のデータを返す
+      print('既存と同じパスワードです。')
+      instance.save()
+      return instance
+
+    # 更新の場合 make_passwordを使ってpasswordをハッシュ化
+    password = validated_data.get('password', instance.password)
+    instance.password = make_password(password)
     instance.save()
+    print('新しいパスワード更新しました。')
     return instance
 
 # UserModelで定義したユーザー作成のメソッドを呼ぶ

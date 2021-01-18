7aa5ce84db82f31c8a6b8b1d51fb6c29249dc478
Your Name <you@example.com>
2021-01-18 09:14:00 +0900

    feat: 必須ではない設定

--- api/serializers/item.py
+++ api/serializers/item.py
@@ -7,7 +7,7 @@ class ItemSerializer(serializers.ModelSerializer):
   store_owner = serializers.ReadOnlyField(source='store_owner.group_ptr_id')
 
   # 画像をBase64で受け取る(decodeする)
-  item_img = Base64ImageField()
+  item_img = Base64ImageField(max_length=None, use_url=True, required=False)
   class Meta:
     model = Item
     fields = ['id', 'item_name', 'item_img', 'item_detail', 'item_price', 'item_total', 'store_owner']

c1b366f10a39c92986f6cf30b2e66005bc8e5b55
Your Name <you@example.com>
2021-02-14 02:07:44 +0900

    feat: RDS、IP設定変更

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # パブリックIPv4アドレス(EIP)/ パブリックIPv4DNS/ CNAME プロパティの値
-ALLOWED_HOSTS = ['13.230.128.244', 'ec2-13-230-128-244.ap-northeast-1.compute.amazonaws.com', 'App-Django-Vue-dev.ap-northeast-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['13.230.53.216', 'ec2-13-230-53-216.ap-northeast-1.compute.amazonaws.com', 'App-Django-Vue-dev.ap-northeast-1.elasticbeanstalk.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'ebdb',
         'USER': 'dbuser',
         'PASSWORD': 'dbpassword',
-        'HOST': 'aa12s152uozykd5.colsjxq8adqw.ap-northeast-1.rds.amazonaws.com',
+        'HOST': 'aa1pjt00ez303v6.colsjxq8adqw.ap-northeast-1.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",

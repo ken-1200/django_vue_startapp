335f77022d0bc0d9e1404b5071e256106e90c6c7
Your Name <you@example.com>
2021-02-15 00:36:01 +0900

    fix: local用に変更

--- app/settings/local.py
+++ app/settings/local.py
@@ -1,10 +1,26 @@
 import os
+import environ
 from .base import *
+
+# settings.pyの位置を起点として３つ上の親ディレクトリを参照
+BASE_DIR = environ.Path(__file__) - 3
+
+env = environ.Env()
+
+# 環境変数でDJANGO_READ_ENV_FILEをTrueにしておくと.envを読んでくれる
+READ_ENV_FILE = env.bool('DJANGO_READ_ENV_FILE', default=True)
+if READ_ENV_FILE:
+    env_file = str(BASE_DIR.path('.env'))
+    env.read_env(env_file)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+# S3共通の設定(開発)
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases

166007cf774cb3b26de49683877628f57f1dd9fc
Your Name <you@example.com>
2021-02-20 11:46:22 +0900

    fix: 削除

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # パブリックIPv4アドレス(EIP)/ パブリックIPv4DNS/ CNAME プロパティの値
-ALLOWED_HOSTS = ['Appmulticontainerdjangovue-env.eba-ycni5mmu.ap-northeast-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 # S3共通の設定(本番)
 if 'AWS_ACCESS_KEY_ID' in os.environ:
@@ -18,12 +18,12 @@ AWS_QUERYSTRING_AUTH = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ebdb',
-        'USER': 'dbuser',
-        'PASSWORD': 'dbpassword',
-        'HOST': 'aa6gfqvq5qbj2w.colsjxq8adqw.ap-northeast-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
